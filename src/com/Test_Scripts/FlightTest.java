91bae80774052425749aa1a1d9bc591685dcbff4
SandipK99 <ksam1599@gmail.com>
2018-06-26 22:03:08 +0530

    all commit

--- src/com/Test_Scripts/FlightTest.java
+++ src/com/Test_Scripts/FlightTest.java
@@ -1,5 +1,6 @@
 package com.Test_Scripts;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.Business_Functions.NewToursFlight;
@@ -7,9 +8,11 @@ import com.Initialize_WebDriver.StartWebDriver;
 
 public class FlightTest extends StartWebDriver
 {
-	@Test(enabled=false)
+	@Test(enabled=true)
 	public static void flightLinkCheck()
 	{
+		//Assert.assertTrue(false);
+		
 		NewToursFlight.flightClick();
 	}
 
